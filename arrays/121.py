e6cb1509e4d84bd9afff56d228911c8850605d97
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-18 23:33:23 +0530

    max profit again

--- arrays/121.py
+++ arrays/121.py
@@ -2,7 +2,7 @@ x = list(map(int, input("Enter: ").split()))
 def minprice(x):
     price = x[0]
     maxprofit = 0
-    for i in range(1, len(x)):
+    for i in range(1,len(x)):
         profit = x[i] - price
         maxprofit = max(maxprofit, profit)
         price = min(price, x[i])
