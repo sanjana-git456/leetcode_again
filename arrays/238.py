2f31c5505fec887f0d86d494868cb5f36d53bb79
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-19 01:32:03 +0530

    product except itself O(n) method

--- arrays/238.py
+++ arrays/238.py
@@ -1,10 +1,11 @@
 x = list(map(int, input("Enter: ").split()))
+prefix = [1] * len(x)
+suffix = [1] * len(x)
 result = []
+for i in range(1, len(x)):
+    prefix[i] = prefix[i-1] * x[i-1]
+for i in range(len(x)-2, -1, -1):
+    suffix[i] = suffix[i+1] * x[i+1]
 for i in range(len(x)):
-    fix = x[i]
-    p = 1
-    for j in range(len(x)):
-        if j != i:
-            p *= x[j]
-    result.append(p)
+    result.append(prefix[i] * suffix[i])
 print(result)
