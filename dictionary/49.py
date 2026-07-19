7f7710003d67eb11832ed5e318948882aebea85e
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-19 22:19:13 +0530

    anagram grouping

---
+++ dictionary/49.py
@@ -0,0 +1,9 @@
+x = input("Enter: ").split()
+result = []
+d = {}
+for i in range(len(x)):
+    a = ''.join(sorted(x[i]))
+    if a not in d:
+        d[a] = []
+    d[a].append(''.join(x[i]))
+print(d)
