26d05fcdf02d751bc4ef4b1a02b14fcc7ebd5006
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-07-23 23:51:12 +0530

    smaller than itself

---
+++ dictionary/1365.py
@@ -0,0 +1,8 @@
+x = list(map(int, input("Enter: ").split()))
+sorted_x = sorted(x)
+d = {}
+for i in range(len(sorted_x)):
+    if sorted_x[i] not in d:
+        d[sorted_x[i]] = i
+result = [d[num] for num in x]
+print(result)
